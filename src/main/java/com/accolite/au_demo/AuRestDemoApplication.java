6518e0d062109825866dbf65abb79d7df5fca83c
Prabakar19 <praphakar119@gmail.com>
2021-01-24 00:19:19 +0530

    Initial Commit

---
+++ src/main/java/com/accolite/au_demo/AuRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.accolite.au_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuRestDemoApplication.class, args);
+	}
+
+}
